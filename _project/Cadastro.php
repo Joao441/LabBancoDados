b3d64c9a9117f0fecf139812e7d49b629d046ae3
Joao441 <51719180+Joao441@users.noreply.github.com>
2019-06-11 15:35:20 -0300

    Create Cadastro.php

---
+++ _project/Cadastro.php
@@ -0,0 +1,26 @@
+<DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta charset="utf-8">
+	   <title> Cadastrar Usuário</title>
+	</head>
+		
+	<body>
+	   
+	   <form method="POST" action="usuario.php">
+			<label>Nome: <\label>
+			<input type="text" name="nome"  placeholder="Digite seu nome "><br>
+		
+			<label>Login: <\label>
+			<input type="text" name="login"  placeholder="Digite seu email"><br>
+			
+			<label>Senha: <\label>
+			<input type ="password" name="senha" ><br>
+			
+			<input type="submit" value="Cadastrar">
+	    </form>
+	</body>	
+</html>		
+		
+		
