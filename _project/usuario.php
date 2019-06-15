973882672886c23df7e981cd2b3df13e552b7c23
Joao441 <51719180+Joao441@users.noreply.github.com>
2019-06-15 13:57:38 -0300

    Update usuario.php
    Implementação do usuario criada

--- _project/usuario.php
+++ _project/usuario.php
@@ -1,10 +1,28 @@
+<html>
+<head>
+<title>Cadastro de Usuario</title>
+</head>
+<body>
+
 <?php
-	//Uses 
-
-	//Variaeis
-
-	//funçãos
-
+	//Variaveis
+	$host = '127.0.0.1';
+	$port = '5432';
+	$dbname = 'LojaOnline';
+	$user = 'gvargas';
+	$password = 'gvargas';
+	$endl = "<br />\n";
 	//implentação
-	
-?>
+	$Connection = "host=".$host." port=".$port." dbname=".$dbname ." user=".$user." password=".$password;
+	$DBConnection = pg_connect($Connection) or die ("Não foi possível conectar ao servidor PostGreSQL".$endl.$endl);
+?>php
+$Nome=$_POST['Nome'];
+$Sobrenome=$_POST['Sobrenome'];
+$CNPJ/CPF=$_POST['CNPJ/CPF'];
+$Endereco=$_POST['Endereco'];
+$Numero=$_POST['Numero'];
+$Bairro=$_POST['Bairro'];
+$CEP=$_POST['CEP'];
+$Usuario=$_POST['Usuario'];
+$Apelido=$_POST['Apelido'];
+</body
